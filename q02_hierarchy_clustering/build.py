5afe1fc720fbdef0107774f9f79d9c4dcb44acfe
pritambhaladhare <pritam.bhaladhare@gmail.com>
2017-11-05 23:00:45 +0000

    Done

--- q02_hierarchy_clustering/build.py
+++ q02_hierarchy_clustering/build.py
@@ -5,8 +5,22 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import scale
 from scipy.cluster import hierarchy
 from sklearn import datasets
+from scipy.cluster.hierarchy import dendrogram, linkage
 
 digits = datasets.load_digits()
 df = pd.DataFrame(scale(digits.data), index=digits.target)
 
 # Write your solution here :
+def hierarchy_clustering(X_train):
+    Z = linkage(X_train, 'average')
+    Z[80]
+    plt.figure(figsize=(25, 10))
+    plt.title('Hierarchical Clustering Dendrogram')
+    plt.xlabel('sample index')
+    plt.ylabel('distance')
+    dendrogram(
+        df,
+        leaf_rotation=90.,  # rotates the x axis labels
+        leaf_font_size=8.,  # font size for the x axis labels
+    )
+    plt.show()
