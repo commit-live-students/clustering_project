4d7417e4aab3fdb688663b1404094ac9e23392cc
satishkhanna <snykhanna@gmail.com>
2017-11-05 14:28:57 +0000

    Done

--- q01_k_means/build.py
+++ q01_k_means/build.py
@@ -10,7 +10,17 @@ X_train = digits.images
 y_train = digits.target
 
 # Write your solution here :
-
-
-
-
+def k_means (X_train,y_train,cluster=10,random_state=9):
+    x=[]
+    y=[]
+    nsamples, nx, ny = X_train.shape
+    d2_train_dataset = X_train.reshape((nsamples,nx*ny))
+    for i in range(1,cluster):
+        km = KMeans(n_clusters=i,random_state=random_state)
+        km.fit(d2_train_dataset)
+        y_pred = km.predict(d2_train_dataset)
+        x.append(i)
+        y.append(y_pred)
+        plt.plot(x,y)
+        plt.show()
+     
