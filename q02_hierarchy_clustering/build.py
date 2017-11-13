636523393cb59ed9ad0fdaa4f7ba5debf9c9d6ca
Sakshi-BUK <sakshi.dhokrat96@gmail.com>
2017-11-13 09:35:24 +0000

    Done

--- q02_hierarchy_clustering/build.py
+++ q02_hierarchy_clustering/build.py
@@ -1,3 +1,4 @@
+# %load q02_hierarchy_clustering/build.py
 # Default imports
 
 import pandas as pd
@@ -10,3 +11,12 @@ digits = datasets.load_digits()
 df = pd.DataFrame(scale(digits.data), index=digits.target)
 
 # Write your solution here :
+def hierarchy_clustering (df):
+    D=hierarchy.linkage(df,'average')
+    plt.figure(figsize=(25,10))
+    plt.title("Hierarchial clustering dendogram")
+    plt.xlabel('sample index')
+    plt.ylabel('distance')
+    hierarchy.dendogram(D,leaf_rotation=90.,leaf_font_size=8.)
+    plt.show()
+#hierarchy_clustering (df)
