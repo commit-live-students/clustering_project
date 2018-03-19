31a0f52b4ecac628a57d2f0601dd071cdc00cf7f
vp999 <patilvish@gmail.com>
2018-03-19 04:57:27 +0000

    Done

--- q01_k_means/build.py
+++ q01_k_means/build.py
@@ -1,6 +1,12 @@
 # Default imports
 from sklearn.cluster import KMeans
+import matplotlib
+matplotlib.use('Agg')
+matplotlib.rcParams['interactive'] == True
+
 import matplotlib.pyplot as plt
+
+
 from sklearn import datasets
 
 
@@ -10,8 +16,14 @@ X_train = digits.images
 y_train = digits.target
 
 # Write your solution here :
+def k_means ( X_train, y_train, cluster = 10,random_state = 9):
+    model = KMeans(init="k-means++", n_clusters=cluster,random_state=random_state )
+    nsamples, nx, ny = X_train.shape
+    X_train = X_train.reshape((nsamples,nx*ny))
+    labels = model.fit_predict ( X_train )
+    plt.scatter(X_train[:, 0], X_train[:, 1], c=labels, s=50, cmap='viridis')
+    return
 
-
-
-
-
+k_means ( X_train, y_train)
+#plt.savefig('D:\\output.pdf', format='pdf', dpi=1200)
+plt.show()
