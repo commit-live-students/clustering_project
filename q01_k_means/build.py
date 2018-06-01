21fe72144199cd3de96a68b22848e5331213448d
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-06-01 07:15:18 +0000

    Done

--- q01_k_means/build.py
+++ q01_k_means/build.py
@@ -1,17 +1,25 @@
 # Default imports
 from sklearn.cluster import KMeans
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn import datasets
+from sklearn.preprocessing import scale
+from sklearn.decomposition import PCA
 
 
 digits = datasets.load_digits()
 
-X_train = digits.images
+X_train = digits.data
 y_train = digits.target
 
 # Write your solution here :
-
-
-
-
-
+def k_means(X_train,y_train,cluster=10,random_state=9):
+    model = KMeans(n_clusters=cluster,random_state=random_state)
+    pca = PCA(n_components=2)
+    reduced_X = pca.fit_transform(X_train)
+    model.fit(reduced_X)
+    ans = model.predict(reduced_X)
+    plt.plot(reduced_X)
+    plt.plot(ans)
+    plt.show()
