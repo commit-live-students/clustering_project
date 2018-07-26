85c5f28739c30b728f9a6cdce70475d36311af1d
SL345 <sherilyn345@gmail.com>
2018-07-26 07:30:05 +0000

    Done

--- q02_hierarchy_clustering/build.py
+++ q02_hierarchy_clustering/build.py
@@ -1,3 +1,4 @@
+# %load q02_hierarchy_clustering/build.py
 # Default imports
 
 import pandas as pd
@@ -11,5 +12,47 @@ digits = datasets.load_digits()
 df = pd.DataFrame(scale(digits.data), index=digits.target)
 
 # Write your solution here :
+def hierarchy_clustering(df):
+    Z_avg = linkage(df, 'average') 
+    Z_single = linkage(df, 'single') 
+    Z_complete = linkage(df, 'complete') 
+    Z_ward = linkage(df, 'ward') 
 
+    plt.figure(figsize=(15, 10))
+    plt.subplot(411)
+    dendrogram(
+        Z_avg,
+        leaf_rotation=90.,  
+        leaf_font_size=3., 
+    )
+    plt.title('Average')
+
+    plt.figure(figsize=(15, 10))
+    plt.subplot(412)
+    dendrogram(
+        Z_single,
+        leaf_rotation=90.,  
+        leaf_font_size=3.,  
+    )
+    plt.title('Single')
+
+    plt.subplot(413)
+    dendrogram(
+        Z_complete,
+        leaf_rotation=90.,  
+        leaf_font_size=3.,  
+    )
+    plt.title('Complete')
+
+    plt.subplot(414)
+    dendrogram(
+        Z_ward,
+        leaf_rotation=90.,  
+        leaf_font_size=3.,  
+    )
+    plt.title('Ward')
+    plt.xlabel('sample index')
+    plt.ylabel('distance')
+
+    plt.show()
 
