5eb7a06a2fd5ec9c12c43c0ef0b088be1d7b9403
HardikaB <hardi.2607@gmail.com>
2018-08-28 13:41:15 +0000

    Done

--- q01_k_means/build.py
+++ q01_k_means/build.py
@@ -1,8 +1,18 @@
+# %load q01_k_means/build.py
 # Default imports
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 from sklearn import datasets
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn.cluster as cluster
+import time
+import numpy as np
+
+sns.set_context('poster')
+sns.set_color_codes()
+plot_kwds = {'alpha' : 0.25, 's' : 80, 'linewidths':0}
 
 
 digits = datasets.load_digits()
@@ -11,8 +21,20 @@ X_train = digits.images
 y_train = digits.target
 
 # Write your solution here :
-
-
-
+def k_means(X_train,y_train,cluster=10,random_state=9):
+    X_train=X_train.reshape(14376,8)
+    start_time = time.time()
+    km = KMeans( n_clusters=cluster,random_state=random_state)
+    km.fit(X_train)
+    labels=km.predict(X_train)   
+    print(km.cluster_centers_)
+    print(km.labels_)
+    print(labels)
+    palette = sns.color_palette('deep', np.unique(labels).max() + 1)
+    colors = [palette[x] if x >= 0 else (0.0, 0.0, 0.0) for x in labels]
+    plt.scatter(X_train.T[0], X_train.T[1], c=colors, **plot_kwds)
+    frame = plt.gca()
+    frame.axes.get_xaxis().set_visible(False)
+    frame.axes.get_yaxis().set_visible(False)
 
 
--- q02_hierarchy_clustering/build.py
+++ q02_hierarchy_clustering/build.py
@@ -1,3 +1,4 @@
+# %load q02_hierarchy_clustering/build.py
 # Default imports
 
 import pandas as pd
@@ -6,10 +7,30 @@ plt.switch_backend('agg')
 from sklearn.preprocessing import scale
 from scipy.cluster import hierarchy
 from sklearn import datasets
-
+from scipy.cluster.hierarchy import dendrogram, linkage
 digits = datasets.load_digits()
 df = pd.DataFrame(scale(digits.data), index=digits.target)
 
+
+
+
+X_train = df
+y_train = df.index
 # Write your solution here :
+def hierarchy_clustering(X_train):
+    # generate the linkage matrix
+    Z = linkage(X_train, 'ward')
+    # calculate full dendrogram
+    plt.figure(figsize=(25, 10))
+    plt.title('Hierarchical Clustering Dendrogram')
+    plt.xlabel('sample index')
+    plt.ylabel('distance')
+    dendrogram(
+        Z,
+        leaf_rotation=90.,  # rotates the x axis labels
+        leaf_font_size=8.,  # font size for the x axis labels
+    )
+
+    plt.show()
 
 
