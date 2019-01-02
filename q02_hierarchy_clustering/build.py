121ee78739b3a4a8cf19dfb5fdbbcb486db076ad
soham009 <sohamthaker17@gmail.com>
2019-01-02 17:16:59 +0000

    Done

--- q02_hierarchy_clustering/build.py
+++ q02_hierarchy_clustering/build.py
@@ -1,5 +1,3 @@
-# Default imports
-
 import pandas as pd
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
@@ -11,5 +9,19 @@ digits = datasets.load_digits()
 df = pd.DataFrame(scale(digits.data), index=digits.target)
 
 # Write your solution here :
+def hierarchy_clustering(df):
+    Z = hierarchy.linkage(df, 'average')
+    plt.figure(figsize=(25, 10))
+    plt.title('Hierarchical Clustering Dendrogram')
+    plt.xlabel('sample index')
+    plt.ylabel('distance')
+    hierarchy.dendrogram(
+    Z,
+    leaf_rotation=90.,  # rotates the x axis labels
+    leaf_font_size=8.,  # font size for the x axis labels
+    )
+    plt.show()
+
+hierarchy_clustering(df)
 
 
