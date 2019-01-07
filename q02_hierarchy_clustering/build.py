8c7c266b519c2cc874eca181c319e8b24b5d931e
tracedence <shoaibrashid887@gmail.com>
2019-01-07 08:54:53 +0000

    Done

--- q02_hierarchy_clustering/build.py
+++ q02_hierarchy_clustering/build.py
@@ -3,11 +3,10 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-plt.switch_backend('agg')
+#plt.switch_backend('agg')
 from sklearn.preprocessing import scale
 from scipy.cluster import hierarchy
 from sklearn import datasets
-
 digits = datasets.load_digits()
 #df = pd.DataFrame(scale(digits.data), index=digits.target)
 # Write your solution here :
@@ -41,31 +40,7 @@ def hierarchy_clustering(X_train):
     plt.show()
 
 hierarchy_clustering(digits.data)
-# plt.subplot(1,4,1)
-# Z = hierarchy.linkage(df,'complete')
-# hierarchy.dendrogram(Z,leaf_rotation=90,leaf_font_size=8)
-# plt.title('Complete linkage')
-# plt.plot()
 
-# plt.subplot(1,4,2)
-# Z = hierarchy.linkage(df,'average')
-# hierarchy.dendrogram(Z,leaf_rotation=90,leaf_font_size=8)
-# plt.title('Average linkage')
-# plt.plot()
 
-# plt.subplot(1,4,3)
-# Z = hierarchy.linkage(df,'single')
-# hierarchy.dendrogram(Z,leaf_rotation=90,leaf_font_size=8)
-# plt.title('Single linkage')
-# plt.plot()
-
-# plt.subplot(1,4,4)
-# Z = hierarchy.linkage(df,'ward')
-# hierarchy.dendrogram(Z,leaf_rotation=90,leaf_font_size=8)
-# plt.title('Ward')
-# plt.plot()
-
-# plt.show()
-print('he')
 
 
