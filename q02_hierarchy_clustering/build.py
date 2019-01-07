0e31ce70e131c6909e799a7a826677d36d4aaeb0
tracedence <shoaibrashid887@gmail.com>
2019-01-07 07:20:05 +0000

    Done

--- q02_hierarchy_clustering/build.py
+++ q02_hierarchy_clustering/build.py
@@ -1,3 +1,4 @@
+# %load q02_hierarchy_clustering/build.py
 # Default imports
 
 import pandas as pd
@@ -8,8 +9,63 @@ from scipy.cluster import hierarchy
 from sklearn import datasets
 
 digits = datasets.load_digits()
-df = pd.DataFrame(scale(digits.data), index=digits.target)
-
+#df = pd.DataFrame(scale(digits.data), index=digits.target)
 # Write your solution here :
+def hierarchy_clustering(X_train):
+    
+    df = pd.DataFrame(scale(X_train))
+    plt.subplot(1,4,1)
+    Z = hierarchy.linkage(df,'complete')
+    hierarchy.dendrogram(Z,leaf_rotation=90,leaf_font_size=8)
+    plt.title('Complete linkage')
+    plt.plot()
+
+    plt.subplot(1,4,2)
+    Z = hierarchy.linkage(df,'average')
+    hierarchy.dendrogram(Z,leaf_rotation=90,leaf_font_size=8)
+    plt.title('Average linkage')
+    plt.plot()
+
+    plt.subplot(1,4,3)
+    Z = hierarchy.linkage(df,'single')
+    hierarchy.dendrogram(Z,leaf_rotation=90,leaf_font_size=8)
+    plt.title('Single linkage')
+    plt.plot()
+
+    plt.subplot(1,4,4)
+    Z = hierarchy.linkage(df,'ward')
+    hierarchy.dendrogram(Z,leaf_rotation=90,leaf_font_size=8)
+    plt.title('Ward')
+    plt.plot()
+
+    plt.show()
+
+hierarchy_clustering(digits.data)
+# plt.subplot(1,4,1)
+# Z = hierarchy.linkage(df,'complete')
+# hierarchy.dendrogram(Z,leaf_rotation=90,leaf_font_size=8)
+# plt.title('Complete linkage')
+# plt.plot()
+
+# plt.subplot(1,4,2)
+# Z = hierarchy.linkage(df,'average')
+# hierarchy.dendrogram(Z,leaf_rotation=90,leaf_font_size=8)
+# plt.title('Average linkage')
+# plt.plot()
+
+# plt.subplot(1,4,3)
+# Z = hierarchy.linkage(df,'single')
+# hierarchy.dendrogram(Z,leaf_rotation=90,leaf_font_size=8)
+# plt.title('Single linkage')
+# plt.plot()
+
+# plt.subplot(1,4,4)
+# Z = hierarchy.linkage(df,'ward')
+# hierarchy.dendrogram(Z,leaf_rotation=90,leaf_font_size=8)
+# plt.title('Ward')
+# plt.plot()
+
+# plt.show()
+print('he')
 
 
