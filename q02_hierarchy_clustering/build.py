66fe163f6b57793649e979d39169d496433d767b
preetiail <ailpreeti@ymail.com>
2019-01-26 13:18:29 +0000

    Done

--- q02_hierarchy_clustering/build.py
+++ q02_hierarchy_clustering/build.py
@@ -1,3 +1,4 @@
+# %load q02_hierarchy_clustering/build.py
 # Default imports
 
 import pandas as pd
@@ -6,10 +7,38 @@ plt.switch_backend('agg')
 from sklearn.preprocessing import scale
 from scipy.cluster import hierarchy
 from sklearn import datasets
-
+from scipy.cluster.hierarchy import dendrogram, linkage
 digits = datasets.load_digits()
 df = pd.DataFrame(scale(digits.data), index=digits.target)
+X_train=df.iloc[:, :-1]
+y_train=df.iloc[:, :-1]
+def hierarchy_clustering(df):
+    Z = linkage(df, 'average')
+    Z1 = linkage(df, 'complete')
+    Z2 = linkage(df, 'single')
+    Z3= linkage(df, 'ward')
+    plt.figure(figsize=(25, 10))
+    plt.subplot(1,1,4)
+    plt.title('Hierarc	hical Clustering Dendrogram')
+    plt.xlabel('sample index')
+    plt.ylabel('distance')
+    dendrogram(Z,leaf_rotation=90,leaf_font_size=8.,) 
+    plt.subplot(1,2,4)
+    plt.title('Hierarc	hical Clustering Dendrogram')
+    plt.xlabel('sample index')
+    plt.ylabel('distance')
+    dendrogram(Z1,leaf_rotation=90,leaf_font_size=8.,)
+    plt.subplot(2,1,4)
+    plt.title('Hierarc	hical Clustering Dendrogram')
+    plt.xlabel('sample index')
+    plt.ylabel('distance')
+    dendrogram(Z2,leaf_rotation=90,leaf_font_size=8.,) 
+    plt.subplot(2,2,4)
+    plt.title('Hierarc	hical Clustering Dendrogram')
+    plt.xlabel('sample index')
+    plt.ylabel('distance')
+    dendrogram(Z3,leaf_rotation=90,leaf_font_size=8.,) 
+    plt.show()
 
-# Write your solution here :
 
 
